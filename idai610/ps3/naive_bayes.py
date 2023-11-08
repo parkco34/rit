d53ad8b5997617eca32a7738e62ac5dfe9657b72
parkco34 <parkercorya@gmail.com>
2023-11-07 19:34:33 -0500

    Just about done with the naive bayes classifier... debugging and error handling next, then on to problem 4.

--- idai610/ps3/naive_bayes.py
+++ idai610/ps3/naive_bayes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import pandas as pd
+import numpy as np
 import re
 import nltk
 from nltk.corpus import stopwords
@@ -13,7 +14,7 @@ PROBLEMS:
     frequency than in the original df["Text"]... ¯\_(⊙_ʖ⊙)_/¯
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 """
-nltk.donwload("stopwords")
+nltk.download("stopwords")
 # A simple list of stop words
 stop_words = set(stopwords.words('english'))
 
@@ -137,35 +138,84 @@ def calculate_likelihoods(feature_matrix, labels, vocabulary, alpha=1):
 
     return likelihoods
 
-def classify(document, priors, likelihoods, vocabulary):
-    wrangled_doc = data_wrangle(document)
-    # frequency vector document vocab
-    doc_vector = Counter(wrangled_doc)
+def classification(document, priors, likelihoods, vocabulary):
+    """
+    Comptues posterior probs for each class given a new document for
+    classification.
+    ------------------------------------------------------------
+    INPUT:
 
-    # Posteriior for each class
+    OUTPUT:
+
+    """
+    # Process document
+    wrangled_doc = data_wrangle(document)
+    # Get counts of each unique word in document
+    doc_vector = Counter(wrangled_doc.split()) # dict
+
     posteriors = {}
+    # Get log2 of priors and log2 of posteriors 
     for class_label in priors:
-        log_posterior = np.log(priors[class_label])
-        # Add log likelihoods of words in doc
+        log_posterior = np.log2(priors[class_label])
+        # Get log2 of likelihoods times count
         for word, count in doc_vector.items():
             if word in vocabulary:
-                log_posterior += np.log(likelihoods[class_label].get(word, 0))\
+                # If None, just stick a zero in there, yo
+                log_posterior += np.log2(likelihoods[class_label].get(word,0))\
                 * count
-        
+
         posteriors[class_label] = log_posterior
+
+    return max(posteriors, key=posteriors.get)
+
+def evaluation_of_model(test_data, test_labels, priors, likelihoods, vocabulary):
+    """
+    Evaluates accuracy, precision, recal and F-1 Score, if it's needed ...
+    ----------------------------------------------------------------------
+    INPUTS:
+        test_data: 
+        test_labels:
+        priors: 
+        likelihoods: 
+        vocabulary: 
+
+    OUTPUTS:
+        accuracy: 
+    """
+    # Number of correct labels
+    correct = 0
+    # Evaluation process
+    for doc, label in zip(test_data, test_labels):
+        predicted = classification(doc, priors, likelihoods, vocabulary)
+        # Compare with true label
+        if predicted == label:
+            correct += 1
+
+    accuracy = correct / len(test_data)
+
+    return accuracy
 
 # Get DataFrame
 df = pd.read_csv(r"Data/dataset_1_review/reviews_polarity_train.csv")
 # Create new column with processed data
 df["wrangled_data"] = df["Text"].apply(data_wrangle)
 labels = df["Label"]
+
+# test data
+test_data = pd.read_csv(r"Data/dataset_1_review/reviews_polarity_test.csv")
+test_labels = test_data["Label"]
+#test_labels["wranlged_test_data"] = test_labels["Label"].apply(data_wrangle)
+
 # unique words
-words = get_words(df["wrangled_data"])
+vocabulary = get_words(df["wrangled_data"])
 feature_matrix = make_feature_matrix(df["wrangled_data"],
-                                     words)
+                                     vocabulary)
 
-likelihood = calculate_likelihoods(feature_matrix, labels, words)
+# Probablilities
 priors = calculate_priors(labels)
+likelihoods = calculate_likelihoods(feature_matrix, labels, vocabulary)
 
+# Accuracy result is 0.006666 NOT GOOD
+accuracy = evaluation_of_model(test_data, test_labels, priors, likelihoods, vocabulary)
 
-#breakpoint()
+breakpoint()
