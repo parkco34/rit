97ccc4fe994bd843bc3e67ae6ba2dc6460430c5e
parkco34 <parkercorya@gmail.com>
2024-04-01 23:38:54 -0400

    Including my final exam, which I will not be able to accomplish because I'm fucking frozen again... ヽ༼ ಠ益ಠ ༽ﾉ

---
+++ spring_24/vis_analgesics/opioid_finalexam.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Data source:
+    https://www.kaggle.com/code/craigchilvers/us-drug-overdose-eda-and-visualisation/input?select=CDC+Injury+Center+Drug+Overdose+Deaths.csv
+"""
+import pandas as pd
+df = pd.read_csv("opioid_dataset.csv", encoding='ISO-8859-1')
+
+#df = pd.read_csv("opioid_data.csv")
+breakpoint()
+
